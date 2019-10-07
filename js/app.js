e2d584c33a9966b632254dc629e8de105ae5232a
Nicole Accardi <naccardi01@wildcats.jwu.edu>
2019-10-07 12:32:39 -0400

    Show-Hide Page Conent
    JavaScript:
    Show-Hide each page's content (#Friday, #Saturday, #Sunday)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+// when the page loads, hide all panels except for the one with the id 'one'
+$('.page').hide();
+// on click, hide the currently active panel and show the one selected
+$('nav').on('click','a',function(e){
+  // don't jump to the panel (the default action)
+  e.preventDefault();
+  // get the id of the selected panel
+  var curr = $(this).attr('href');
+  // hide all the panels
+  $('.page').hide();
+  // show the selected panel
+  $(curr).show();
+});
+
